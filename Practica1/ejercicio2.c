ce784b6da0443c548855b43ef90de21bbff3703e
cesarRz <cesarrz2000@gmail.com>
2023-01-31 17:54:18 -0600

    Ejercicio 2

--- Practica1/ejercicio2.c
+++ Practica1/ejercicio2.c
@@ -4,67 +4,87 @@
 #else
 #include <GL/glut.h>
 #endif
+#include <stdio.h>
 #include <math.h>
 
-void ajusta () {
+void ajusta()
+{
     glClearColor(0.9, 0.9, 0.9, 0.0);
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
     gluOrtho2D(0, 640, 0, 480);
 }
 
-void dibuja(void)
+void dibuja()
 {
-    GLboolean colorBlanco = GL_TRUE;  
-    float azul[]={0.0, 0.0, 0.9};
-    float negro[]={0.0, 0.0, 0.0};
     glClear(GL_COLOR_BUFFER_BIT);
-    // Habilitamos la linea punteada
-    glEnable(GL_LINE_STIPPLE);
-    glLineStipple(2, 0xCCCC);
-    for (int i = 1; i < (640); i += 20){
-        for (int j = 1; j < (480); j += 20){
-            glColor3fv(azul);
-            glBegin(GL_LINE_LOOP);
-                glVertex2i(i, j);
-                glVertex2i(i, j + 20);
-                glVertex2i(i + 20, j + 20);
-                glVertex2i(i + 20, j);
-            glEnd();
-            // glRecti(i, j, i + 20, j + 20);
-        }
-    }
-    glDisable(GL_LINE_STIPPLE);
-    //Cambiamos a color negro
-    glColor3fv(negro);
-    // Creamos el eje
+    
+    // Cuadrados de tabla
     glLineWidth(2);
+    glEnable(GL_LINE_STIPPLE);
+    glLineStipple(2,0x0F0F);
     glBegin(GL_LINES);
-        glVertex2i(0, 240);
-        glVertex2i(640, 240);
-        glVertex2i(320, 480);
-        glVertex2i(320, 0);
-    glEnd();
-    // Graficamos coseno
-    glPointSize(3);
-    glBegin(GL_LINE_STRIP);
-        for (float i = -1.5; i <= 1.5; i+= 0.1)
+        glColor3ub(0,0,255);
+        for (int i = 0; i <= 640; i+=20)
         {
-            float x = (float)i*640 + 320 ; 
-            float y = 480 * cos(i);
-            glVertex2f(x, y);
+            glVertex2i(i,480);
+            glVertex2i(i,0);
+
         }
     glEnd();
+    glBegin(GL_LINES);
+        glColor3ub(0,0,100);
+        for (int i = 0; i <= 480; i+=20)
+        {
+            glVertex2i(640,i);
+            glVertex2i(0,i);
+
+        }
+    glEnd();
+    glDisable(GL_LINE_STIPPLE);
+
+    // Eje de Tabla
+    glLineWidth(5);
+    glBegin(GL_LINES);
+        glColor3ub(0,0,0);
+            glVertex2i(320,0);
+            glVertex2i(320,640);
+
+            glVertex2i(0,240);
+            glVertex2i(640,240);
+    glEnd();
+
+
+    // Coseno
+    glBegin(GL_LINE_STRIP);
+    glLineWidth(3);
+    glColor3ub(255,0,0);
+    float a = 3.14;
+    for (float x = (a*-1); x <= a; x+= .1)
+        {
+            
+            float x_escalado = (float)x* (640 / (2*a)) + 320; 
+            float y = cos(x);
+            float y_escalado = y * 240 + 240;
+            
+            glVertex2f(x_escalado, y_escalado);
+
+        }   
+    glEnd();
+
+
+
+
     glFlush();
 }
 
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
-    glutInitWindowSize(640,480);
+    glutInitWindowSize(640, 480);
     glutInitWindowPosition(100, 150);
-    glutCreateWindow("Titulo");
+    glutCreateWindow("Coseno");
     glutReshapeFunc(ajusta);
     glutDisplayFunc(dibuja);
     glutMainLoop();
