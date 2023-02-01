134c4e5d16068ddf55c505b22d1a7bd3576c53df
cesarRz <cesarrz2000@gmail.com>
2023-02-01 10:11:37 -0600

    dynamic cycle

--- Practica1/ejercicio2.c
+++ Practica1/ejercicio2.c
@@ -63,7 +63,7 @@ void dibuja()
     for (float x = (a*-1); x <= a; x+= .1)
         {
             
-            float x_escalado = (float)x* (640 / (2*a)) + 320; 
+            float x_escalado = (float)x* (640 / (2*a)) + 320;
             float y = cos(x);
             float y_escalado = y * 240 + 240;
             
@@ -71,10 +71,6 @@ void dibuja()
 
         }   
     glEnd();
-
-
-
-
     glFlush();
 }
 
