3fdbf03d684b4ee89b82133bddeb6a12b3dffd08
cesarRz <cesarrz2000@gmail.com>
2023-02-16 18:49:23 -0600

    Ejercicio 1 terminado

--- Tareas-Grupales/Tarea3/ejercicio1.c
+++ Tareas-Grupales/Tarea3/ejercicio1.c
@@ -5,6 +5,58 @@
 #include <GL/glut.h>
 #endif
 
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+struct Punto{
+   int cx;
+   int cy;
+   int color[3];
+   int radio;
+   struct Punto *sig;
+};
+
+typedef struct Punto Punto;
+
+// Funciones para manejo de estructuras
+struct Punto *ultimaPunto(Punto *lista){
+    Punto *p;
+    p = lista;
+    while (p->sig != NULL)
+    {
+        p = p->sig;
+    }
+    return p;
+}
+void agregaPunto(Punto *anterior, Punto *nuevo){
+    if (anterior->sig == NULL)
+    {
+        anterior->sig = nuevo;
+        nuevo->sig = NULL;
+    }
+    else
+    {
+        nuevo->sig = anterior->sig;
+        anterior->sig = nuevo;
+    }
+}
+Punto *creaPunto(int cx, int cy,int clr[3]){
+    Punto *p;
+    p = (Punto *)malloc(sizeof(Punto));
+    p->cx = cx;
+    p->cy = cy;
+    p->color[0] = clr[0];
+    p->color[1] = clr[1];
+    p->color[2] = clr[2];
+    p->sig = NULL;
+    return p;
+}
+
+int color[3] = {255,0,0};
+Punto *p = NULL;
+int clicks = 1;
+
 void ajusta () {
     glClearColor(0.9, 0.9, 0.9, 0.0);
     glMatrixMode(GL_PROJECTION);
@@ -13,10 +65,9 @@ void ajusta () {
 }
 
 void Bresenham(int centro_x, int centro_y, int radio){
-    int e, x,y = 0;
-    y = radio;
-
-    e = 3 -2*radio;
+    int x = 0;
+    int y = radio;
+    int e = 3 - 2 * radio;
 
     glBegin(GL_POINTS);
     while (x < y)
@@ -51,7 +102,6 @@ void Bresenham(int centro_x, int centro_y, int radio){
         glVertex2i(centro_x - x, centro_y - y );
         glVertex2i(centro_x - y, centro_y - x );
     }
-    
 
 
     glEnd();
@@ -59,9 +109,76 @@ void Bresenham(int centro_x, int centro_y, int radio){
 }
 
 void dibuja(){
+    
+    Punto *a;
+    a = p;
     glClear(GL_COLOR_BUFFER_BIT);
-    Bresenham(320,240, 50);
+
+    if (a != NULL)
+    {
+        while (a != NULL)
+        {
+            glColor3ub(a->color[0], a->color[1], a->color[2]);
+            Bresenham(a->cx,a->cy, a->radio);
+            a = a->sig;
+        }
+        
+    }
     glFlush();
+}
+
+void motion(int x, int y){
+    int alto_ventana = 480;
+    Punto *nuevo;
+    nuevo = ultimaPunto(p);
+    nuevo->radio = sqrt( pow(x - nuevo->cx, 2) + pow(alto_ventana-y - nuevo->cy, 2));
+    glutPostRedisplay();
+
+}
+void raton(int boton, int estado, int x, int y){
+    int alto_ventana = 480;
+    Punto *nuevo;
+
+    if(boton == GLUT_LEFT_BUTTON && estado == GLUT_DOWN)    {
+
+            nuevo = creaPunto(x, alto_ventana-y, color); 
+            if (p == NULL){
+                    p = nuevo;
+            }
+            agregaPunto(ultimaPunto(p), nuevo);   
+    }
+}
+void teclado(unsigned char tecla, int x, int y){
+    switch(tecla) {
+        case 27:	
+            exit(0);		// Esc =>Salir del programa
+            break;
+        case 'r':
+            color[0] = 255;
+            color[1] = 0;
+            color[2] = 0; 
+            
+		    break;
+        case 'b':
+            color[0] = 0;
+            color[1] = 0;
+            color[2] = 255;  //azul
+            
+			break;
+        case 'g':
+            color[0] = 0;
+            color[1] = 255;
+            color[2] = 0;  //verde
+            
+            break;
+        case 'o':
+            color[0] = 255;
+            color[1] = 144;
+            color[2] = 0;  //naranja
+            
+            break;
+    }
+
 }
 
 int main(int argc, char** argv)
@@ -70,8 +187,11 @@ int main(int argc, char** argv)
     glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
     glutInitWindowSize(640,480);
     glutInitWindowPosition(100, 150);
-    glutCreateWindow("Ejercicio 1");
+    glutCreateWindow("Ejercicio 1: Bresenham");
     glutReshapeFunc(ajusta);
+    glutMouseFunc(raton);
+    glutMotionFunc(motion);
+    glutKeyboardFunc(teclado);
     glutDisplayFunc(dibuja);
     glutMainLoop();
     return 0;
