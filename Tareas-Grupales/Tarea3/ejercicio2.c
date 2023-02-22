6087a0395b271acb2b6a60555eb0278b16b88a07
cesarRz <cesarrz2000@gmail.com>
2023-02-21 18:12:50 -0600

    Ejercicio 2 sin Algoritmo

--- Tareas-Grupales/Tarea3/ejercicio2.c
+++ Tareas-Grupales/Tarea3/ejercicio2.c
@@ -16,34 +16,6 @@ int puntos = 0;
 GLboolean fin = GL_FALSE;
 
 
-// Ray Casting Algorithm
-int point_in_polygon(int point_x, int point_y, int poly_x[], int poly_y[], int n)
-{
-    int num_intersections = 0;
-    int i;
-    for (i = 0; i < n; i++)
-    {
-        int x1 = poly_x[i];
-        int y1 = poly_y[i];
-        int x2 = poly_x[(i+1)%n];
-        int y2 = poly_y[(i+1)%n];     
-
-        if( point_y > fmin(y1, y2) && point_y <= fmax(y1, y2) &&  point_x <= fmax(x1, x2) && y1 != y2 ) {
-            
-            float x_intersection = (point_y-y1)*(x2-x1)/(y2-y1)+x1;
-                if (x1 == x2 || point_x <= x_intersection)
-                {
-                    num_intersections++;
-                }
-        }
-
-        
-    }
-    return num_intersections % 2 == 1;
-}
-
-
-
 void ajusta(int ancho, int alto)
 {
     glClearColor(1.0, 1.0, 1.0, 0.0);
@@ -89,6 +61,7 @@ void termina(int tecla, int x, int y)
         break;
     case GLUT_KEY_DOWN:
         puntos = 0;
+        index_fin = 0;
         fin = GL_FALSE;
         break;
     case GLUT_KEY_LEFT:
@@ -105,9 +78,18 @@ void teclado(unsigned char tecla, int x, int y)
 
 bool ccw(int a, int b, int c)
 {
-    double area2 = (pos_x[b] - pos_x[a]) * (pos_y[c] - pos_y[a]) - (pos_y[b] - pos_y[a]) * (pos_x[c] - pos_x[a]);
-    return area2 >= 0;
+        double area2 = (pos_x[b] - pos_x[a]) * (pos_y[c] - pos_y[a]) - (pos_y[b] - pos_y[a]) * (pos_x[c] - pos_x[a]);
+        return area2 >= 0;
+    
 }
+
+bool ccw_fin(int a, int b, int c)
+{
+        double area2 = (pos_x[b] - pos_x[a]) * (puntos_fin[c][1] - pos_y[a]) - (pos_y[b] - pos_y[a]) * (puntos_fin[c][0] - pos_x[a]);
+        return area2 >= 0;
+    
+}
+    
 void dibuja(void)
 {
     glClear(GL_COLOR_BUFFER_BIT);
@@ -131,11 +113,20 @@ void dibuja(void)
     glEnd();
 
 
+    
+
     // Dibuja puntos extras
         glBegin(GL_POINTS);
     if (index_fin != 0){
         for (i = 0; i < index_fin; i++){
-            if (point_in_polygon(puntos_fin[i][0],puntos_fin[i][1],pos_x, pos_y, puntos))
+            GLboolean dentro = GL_TRUE;
+            for (size_t j = 0; j < puntos; j++)
+            {
+                if(!ccw_fin(j,j+1,i))
+                    dentro = GL_FALSE;
+            }
+
+            if (dentro)
             {
                 glColor3ub(0,255,0);
             }else{
