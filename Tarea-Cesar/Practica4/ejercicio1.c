5f126e572dc6e720034df45cfcabedc8394f29fd
cesarRz <cesarrz2000@gmail.com>
2023-03-01 17:51:56 -0600

    Ejercicio 1 Terminado Practica 4

--- Tarea-Cesar/Practica4/ejercicio1.c
+++ Tarea-Cesar/Practica4/ejercicio1.c
@@ -12,33 +12,21 @@
 #include "matriz.h"
 
 int tamano_lateral = 100;
-float coordenadas[4][2];
-
-void assign_escalamiento(float x, float y, float m[3][3]) // crea la matriz de escalamiento
-{
-    for (int i = 0; i < 3; i++)
-        for (int j = 0; j < 3; j++)
-            if (i == 0  && j == 0)
-                m[i][j] = x;
-            else if (i == 1 && j == 1)
-                m[i][j] = x;
-            else if ((i == 2 && j == 2))
-                m[i][j] = 1;
-            else
-                m[i][j] = 0;
-
-}
-
+float coordenadas[4][3];
 
 void assign_base(int base_x, int base_y){
     coordenadas[0][0] = base_x;
     coordenadas[0][1] = base_y;
+    coordenadas[0][2] = 1;
     coordenadas[1][0] = base_x + tamano_lateral;
     coordenadas[1][1] = base_y;
+    coordenadas[1][2] = 1;
     coordenadas[2][0] = ((tamano_lateral * 0.2) + base_x + tamano_lateral);
     coordenadas[2][1] = (base_y + (tamano_lateral / 2));
+    coordenadas[2][2] = 1;
     coordenadas[3][0] = ((tamano_lateral * 0.2) + base_x);
     coordenadas[3][1] = (base_y + (tamano_lateral / 2));
+    coordenadas[3][2] = 1;
 }
 
 float Traslacion[3][3];
@@ -62,14 +50,28 @@ void creaT(float dx, float dy) {
 }
 
 
+void assign_escalamiento(float x, float y) 
+{
+    for (int i = 0; i < 3; i++)
+        for (int j = 0; j < 3; j++)
+            if (i == 0  && j == 0)
+                Escalamiento[i][j] = x;
+            else if (i == 1 && j == 1)
+                Escalamiento[i][j] = y;
+            else if ((i == 2 && j == 2))
+                Escalamiento[i][j] = 1;
+            else
+                Escalamiento[i][j] = 0;
+
+}
+
 void dibuja_romboide(){
 
     glColor3ub(0,0,255);
 
     glBegin(GL_QUADS);
 
-    for (int i = 0; i < 4; i++)
-    {
+    for (int i = 0; i < 4; i++){
         glVertex2d(coordenadas[i][0], coordenadas[i][1]);
     }
 
@@ -151,23 +153,41 @@ void teclado(unsigned char tecla, int x, int y){
     glutPostRedisplay();
 }
 
-// void teclado_especial(int tecla, int x, int y){
+void teclado_especial(int tecla, int x, int y){
     
-//     int mr[3][3];
-//     switch (tecla)
-//     {
-//     case GLUT_KEY_DOWN:
-//         assign_escalamiento(0.8, 0.8, Escalamiento);
-//         multiplica(3,4,3,coordenadas, Escalamiento, mr);
     
-//     break;
+    switch (tecla)
+    {
+    case GLUT_KEY_DOWN:
+        assign_escalamiento(0.8, 0.8);
+        printf("Coordenadas\n");
+        imprime(4,2,coordenadas);
+        printf("Escalamiento\n");
+        imprime(3,3,Escalamiento);
+        multiplica(3,4,3,coordenadas, Escalamiento, coordenadas);
+        
+        printf("Coordenadas\n");
+        imprime(4,2,coordenadas);
 
-//     case GLUT_KEY_UP:
+    break;
 
-//     break;
-//     }
+    case GLUT_KEY_UP:
 
-// }
+        assign_escalamiento(1.2, 1.2);
+        printf("Coordenadas\n");
+        imprime(4,2,coordenadas);
+        printf("Escalamiento\n");
+        imprime(3,3,Escalamiento);
+        multiplica(3,4,3,coordenadas, Escalamiento, coordenadas);
+        
+        printf("Coordenadas\n");
+        imprime(4,3,coordenadas);
+
+    break;
+    }
+
+    glutPostRedisplay();
+}
 
 void dibuja(){
     glClear(GL_COLOR_BUFFER_BIT);
@@ -189,7 +209,7 @@ int main(int argc, char** argv)
     glutInitWindowPosition(100, 150);
     glutCreateWindow("Ejercicio 1");
     glutKeyboardFunc(teclado);
-    // glutKeyboardFunc(teclado_especial);
+    glutSpecialFunc(teclado_especial);
     glutReshapeFunc(ajusta);
     glutDisplayFunc(dibuja);
     glutMainLoop();
