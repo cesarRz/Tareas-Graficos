42d3a54cd095eb1daa951b3d178e9acf580f86ca
cesarRz <cesarrz2000@gmail.com>
2023-03-01 22:10:17 -0600

    Ejercicio 3

---
+++ Tarea-Cesar/Practica4/ejercicio3.c
@@ -0,0 +1,173 @@
+#ifdef __APPLE__
+#define GL_SILENCE_DEPRECATION
+#include <GLUT/glut.h>
+#else
+#include <GL/glut.h>
+#endif
+
+
+int paleta[4][4]={{192, 192, 192, 255},{255, 0, 0, 255},{255, 255, 0, 255},{0, 255, 0, 255}};
+int color_rojo[4] = {192, 192, 192, 255};
+int color_verde[4] = {192, 192, 192, 255};
+int color_amarillo[4] = {192, 192, 192, 255};
+
+int foco = 0;
+
+void ajusta () {
+    glClearColor(0.9, 0.9, 0.9, 0.0);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    // gluOrtho2D(0, 640, 0, 480);
+    glOrtho(0,640,0,480,-200,200);
+}
+
+void animaT(int v){
+
+    switch (foco)
+    {
+    case 0:
+        color_amarillo[0] = paleta[0][0];
+        color_amarillo[1] = paleta[0][1];
+        color_amarillo[2] = paleta[0][2];
+        color_amarillo[3] = paleta[0][3];
+
+        color_rojo[0] = paleta[1][0];
+        color_rojo[1] = paleta[1][1];
+        color_rojo[2] = paleta[1][2];
+        color_rojo[3] = paleta[1][3];
+        glutTimerFunc(2000, animaT, 0);
+        foco ++;
+        break;
+    case 1:
+        color_rojo[0] = paleta[0][0];
+        color_rojo[1] = paleta[0][1];
+        color_rojo[2] = paleta[0][2];
+        color_rojo[3] = paleta[0][3];
+
+        color_verde[0] = paleta[3][0];
+        color_verde[1] = paleta[3][1];
+        color_verde[2] = paleta[3][2];
+        color_verde[3] = paleta[3][3];
+
+        glutTimerFunc(2000, animaT, 0);
+        glutPostRedisplay();
+
+        foco ++;
+        break;
+
+    case 2:
+        color_verde[0] = paleta[0][0];
+        color_verde[1] = paleta[0][1];
+        color_verde[2] = paleta[0][2];
+        color_verde[3] = paleta[0][3];
+
+        glutTimerFunc(500, animaT, 0);
+
+        foco ++;
+        break;
+    case 3:
+        color_verde[0] = paleta[3][0];
+        color_verde[1] = paleta[3][1];
+        color_verde[2] = paleta[3][2];
+        color_verde[3] = paleta[3][3];
+
+        glutTimerFunc(500, animaT, 0);
+
+        foco ++;
+        break;
+
+    case 4:
+        color_verde[0] = paleta[0][0];
+        color_verde[1] = paleta[0][1];
+        color_verde[2] = paleta[0][2];
+        color_verde[3] = paleta[0][3];
+
+        glutTimerFunc(500, animaT, 0);
+
+        foco ++;
+        break;
+
+    case 5:
+        color_verde[0] = paleta[3][0];
+        color_verde[1] = paleta[3][1];
+        color_verde[2] = paleta[3][2];
+        color_verde[3] = paleta[3][3];
+
+        glutTimerFunc(500, animaT, 0);
+
+        foco ++;
+        break;
+    case 6:
+        foco = 0;
+
+        color_verde[0] = paleta[0][0];
+        color_verde[1] = paleta[0][1];
+        color_verde[2] = paleta[0][2];
+        color_verde[3] = paleta[0][3];
+
+        color_amarillo[0] = paleta[2][0];
+        color_amarillo[1] = paleta[2][1];
+        color_amarillo[2] = paleta[2][2];
+        color_amarillo[3] = paleta[2][3];
+
+        glutTimerFunc(1000, animaT, 0);
+
+        break;
+
+    default:
+        break;
+    }
+
+    glutPostRedisplay();
+
+
+}
+
+void dibuja_circulo(int base_x, int base_y, int color[4]){
+
+    glColor4ub(color[0], color[1], color[2], color[3]);
+
+    glPushMatrix();
+        glTranslatef(base_x, base_y, 1);
+        glutSolidSphere(35, 200, 200);
+    glPopMatrix();
+
+
+}
+
+void dibuja_cubo(){
+    glColor3ub(255, 172, 51);
+    glPushMatrix();
+        glTranslatef(300, 245, 1);
+        glScalef(1.1, 3, 1);
+        glutSolidCube(100);
+    glPopMatrix();
+}
+
+void dibuja(){
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    dibuja_cubo();
+
+
+
+    dibuja_circulo(300, 340, color_rojo);
+    dibuja_circulo(300, 240, color_amarillo);
+    dibuja_circulo(300, 140, color_verde);
+    
+    glFlush();
+}
+
+int main(int argc, char** argv)
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+    glutInitWindowSize(640,480);
+    glutInitWindowPosition(100, 150);
+    glutCreateWindow("Ejercicio 3");
+    glutReshapeFunc(ajusta);
+    glutDisplayFunc(dibuja);
+    glutTimerFunc(2, animaT, 0);
+    glutMainLoop();
+    return 0;
+}
